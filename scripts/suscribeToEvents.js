315eb1b51321498be87a7f62288d1abd29dcd99b
asanson <alexandresanson.pro@gmail.com>
2023-11-24 16:08:19 +0100

    feat: Script to suscribe to all SmartDeploy contract's events

---
+++ scripts/suscribeToEvents.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+const MERCURY_ACCESS_TOKEN = process.env.MERCURY_ACCESS_TOKEN;
+const MERCURY_BACKEND_ENDPOINT = process.env.MERCURY_BACKEND_ENDPOINT;
+const SMARTDEPLOY_CONTRACT = process.env.SMARTDEPLOY_CONTRACT;
+
+const suscribe = async () => {
+    try {
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${MERCURY_ACCESS_TOKEN}`
+            }
+        };
+
+        const data = {
+            contract_id: SMARTDEPLOY_CONTRACT,
+            max_single_size: 2000
+        };
+
+        const response = await axios.post(`${MERCURY_BACKEND_ENDPOINT}/event`, data, config);
+
+        console.log(response.data);
+
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+    }
+};
+
+suscribe();
