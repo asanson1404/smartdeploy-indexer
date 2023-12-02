e45f533dd0a05961dd9150d79efa1e4a93c1fd63
asanson <alexandresanson.pro@gmail.com>
2023-12-02 15:54:32 +0100

    fix: Correct xdr production

--- scripts/getEventSubscription.js
+++ scripts/getEventSubscription.js
@@ -2,7 +2,7 @@ const { runQuery } = require("./runQuery");
 const sorobanClient = require('soroban-client');
 
 const contractAddress = process.env.SMARTDEPLOY_CONTRACT;
-const xdrTopic1 = sorobanClient.nativeToScVal("deploy").toXDR("base64");
+const xdrTopic1 = sorobanClient.xdr.ScVal.scvSymbol("deploy").toXDR("base64");
 
 const query = `query DeployEvents {
     eventByTopic(t1: "${xdrTopic1}") {
