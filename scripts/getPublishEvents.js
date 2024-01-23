4db39abba47a76eac98f5e992bdefbfa1824e8e0
asanson <alexandresanson.pro@gmail.com>
2024-01-23 17:33:35 +0800

    feat: Subscribe to and get "publish" events

---
+++ scripts/getPublishEvents.js
@@ -0,0 +1,29 @@
+const { runQuery } = require("./runQuery");
+require("dotenv").config();
+const sorobanClient = require('soroban-client');
+
+const contractAddress = process.env.SMARTDEPLOY_CONTRACT;
+const xdrTopic1 = sorobanClient.xdr.ScVal.scvSymbol("publish").toXDR("base64");
+
+const query = `query PublishEvents {
+    eventByTopic(t1: "${xdrTopic1}") {
+        edges {
+            node {
+                contractId
+                data
+                ledger
+                ledgerTimestamp
+                topic1
+                topic2
+                topic3
+                topic4
+            }
+        }
+    }   
+}`;
+
+const variables = {
+    
+};
+
+runQuery(query, variables);
---
+++ scripts/subscribePublishEvent.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+require("dotenv").config();
+const sorobanClient = require('soroban-client');
+
+const MERCURY_ACCESS_TOKEN = process.env.MERCURY_ACCESS_TOKEN;
+const MERCURY_BACKEND_ENDPOINT = process.env.MERCURY_BACKEND_ENDPOINT;
+const SMARTDEPLOY_CONTRACT = process.env.SMARTDEPLOY_CONTRACT;
+
+const subscribe_publish = async () => {
+    try {
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${MERCURY_ACCESS_TOKEN}`
+            }
+        };
+
+        const data = {
+            contract_id: SMARTDEPLOY_CONTRACT,
+            topic1: sorobanClient.xdr.ScVal.scvSymbol("publish").toXDR("base64"),
+            max_single_size: 2000
+        };
+
+        const response = await axios.post(`${MERCURY_BACKEND_ENDPOINT}/event`, data, config);
+
+        if (response.status == 200) {
+            console.log("success");
+        }
+        //console.log(response);
+
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+    }
+};
+
+subscribe_publish();
